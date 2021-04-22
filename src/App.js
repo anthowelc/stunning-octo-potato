7353bb9f15e62de5e44644452df188daf403fa80
anthowelc <welc.anthony@gmail.com>
2021-04-22 13:01:18 +0200

    add hello function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [text, setText] = useState('Default')
+
+  const getText = async () => {
+    const data = await fetch('/.netlify/functions/hello')
+    const json = await data.json()
+    const text = json.message
+
+    setText(text)
+  }
+
+  const hanldeClick = () => {
+    getText()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+
+        <h2>{text}</h2>
+        <button onClick={hanldeClick}>CLIIIIIK</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
